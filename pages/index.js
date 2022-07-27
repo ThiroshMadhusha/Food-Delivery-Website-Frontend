91fb41e3eb9f5c241b895e0654934c41c7a49145
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-07-27 21:45:18 +0530

    Complete main index page

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,11 @@ import Layout from "../components/Layout";
 import Hero from "../components/Hero";
 import css from "../styles/Home.module.css";
 import Services from "../components/Services";
+import { client } from "../lib/client";
+import Menu from "../components/Menu";
 
-export default function Home() {
+export default function Home({ pizzas }) {
+  // console.log(pizzas);
   return (
     <Layout>
       <div className={css.container}>
@@ -17,8 +20,19 @@ export default function Home() {
         <main>
           <Hero />
           <Services />
+          <Menu pizzas={pizzas} />
         </main>
       </div>
     </Layout>
   );
 }
+
+export const getServerSideProps = async () => {
+  const query = '*[_type=="pizza"]';
+  const pizzas = await client.fetch(query);
+  return {
+    props: {
+      pizzas,
+    },
+  };
+};
